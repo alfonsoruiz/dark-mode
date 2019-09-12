3d7c699bff0f3cc1b3f2b0f64c1bb22694e636ad
alfonsoruiz <1800ruiz@gmail.com>
2019-09-11 23:17:37 -0700

    cleaned up spacing in compnent and hook files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,12 @@ import useDarkMode from '../hooks/useDarkMode';
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useDarkMode(false);
+
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
+
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
